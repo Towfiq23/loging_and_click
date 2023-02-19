898cbce97902cc506911c9498f6b960c7d4fdb0c
towfiq23 <125779503+Towfiq23@users.noreply.github.com>
2023-02-19 04:43:12 +0000

    first_code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 def get_drvier():
   # Set options to make browsing easier
@@ -11,12 +13,15 @@ def get_drvier():
   options.add_argument("disable-blink-features=AutomationControlled")
 
   driver = webdriver.Chrome(options=options)
-  driver.get("https://www.aptiv.com/en/insights/autonomous-driving")
+  driver.get("http://automated.pythonanywhere.com/login/")
   return driver
 
 def main():
   driver = get_drvier()
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div[2]/div[2]/div[2]/div/div/div/div[1]")
-  return element.text
+  driver.find_element(by="id", value="id_username").send_keys("automated")
+  time.sleep(2)
+  driver.find_element(by="id", value="id_password").send_keys("automatedautomated"+ Keys.RETURN)
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+  print(driver.current_url)
 
 print(main())
